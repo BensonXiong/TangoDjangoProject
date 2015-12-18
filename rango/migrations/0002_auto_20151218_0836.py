3de47cbd9cf42d38c328b10c421981b36a7bde09
BensonXiong <benson_xiong@sina.com>
2015-12-18 18:03:27 +0800

    b

---
+++ rango/migrations/0002_auto_20151218_0836.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='likes',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(default=datetime.datetime(2015, 12, 18, 8, 36, 2, 751160, tzinfo=utc), unique=True),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='views',
+            field=models.IntegerField(default=0),
+        ),
+    ]
--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from rango.models import Category
 # Create your views here.
 def index(request):
-	category_list = Category.objects.order_by('-likes')[:5]
+	category_list = Category.objects.order_by('-name')[:5]
 	context_dict = {'categories':category_list}
-	return render(request,'rango/index.html',context)
+	return render(request,'rango/index.html',context_dict)
